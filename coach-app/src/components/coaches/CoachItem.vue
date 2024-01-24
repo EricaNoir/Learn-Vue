98324a90c586fb57f18fc0e361f65772528c4a68
EricaHu <xihu4221@uni.sydney.edu.au>
2024-01-24 14:03:52 +0800

    coach filter

--- coach-app/src/components/coaches/CoachItem.vue
+++ coach-app/src/components/coaches/CoachItem.vue
@@ -20,10 +20,10 @@ export default {
             return `${this.firstName} ${this.lastName}`;
         },
         coachContactLink() {
-            return `${this.$router.path}/${this.id}/contact`;
+            return `${this.$route.path}/${this.id}/contact`;
         },
         coachDetailsLink() {
-            return `${this.$router.path}/${this.id}`;
+            return `${this.$route.path}/${this.id}`;
         },
     },
 };
