59a4992b6b881bae922142275c91237b24536695
EricaHu <xihu4221@uni.sydney.edu.au>
2024-01-25 18:03:16 +0800

    adding coach to vuex

--- coach-app/src/pages/coaches/CoachRegistration.vue
+++ coach-app/src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,23 @@
 <template>
-    REGISTER
+    <section>
+        <BaseCard>
+            <h2>Register as a coach now!</h2>
+            <CoachForm @save-data="saveData"></CoachForm
+        ></BaseCard>
+    </section>
 </template>
+
+<script>
+import CoachForm from "../../components/coaches/CoachForm.vue";
+export default {
+    components: {
+        CoachForm,
+    },
+    methods: {
+        saveData(data) {
+            this.$store.dispatch("coaches/registerCoach", data);
+            this.$router.replace("/coaches");
+        },
+    },
+};
+</script>
