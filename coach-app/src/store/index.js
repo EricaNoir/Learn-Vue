9bfcfdd3fc0392a0fce89e86b3b10f490b9a6d2e
EricaHu <xihu4221@uni.sydney.edu.au>
2024-02-05 15:15:42 +0800

    request

--- coach-app/src/store/index.js
+++ coach-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from "vuex";
 import coachesModule from "./modules/coaches/index.js";
+import requestsModule from "./modules/requests/index.js";
 
 const store = createStore({
     modules: {
         coaches: coachesModule,
+        requests: requestsModule,
     },
     state() {
         return {
@@ -13,8 +15,8 @@ const store = createStore({
     getters: {
         userId(state) {
             return state.userId;
-        }
-    }
+        },
+    },
 });
 
 export default store;
